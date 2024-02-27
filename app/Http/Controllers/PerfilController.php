6bb9d5db4ac67b54e74079b0e5ddff2d1353b46c
JLXP <javier.xool@outlook.com>
2024-02-27 13:56:10 -0600

    finish proyect devstagram

---
+++ app/Http/Controllers/PerfilController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Imagick\Driver;
+
+class PerfilController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('perfil.index');
+    }
+
+    public function store(Request $request)
+    {
+        $request->request->add(['username' => Str::slug($request->username)]);
+
+        $this->validate($request, [
+            'username' => ['required', 'unique:users,username,' . auth()->user()->id, 'min:3', 'max:20', 'not_in:twitter, editar-perfil'],
+        ]);
+
+        if ($request->imagen) {
+            //Leer imagenes
+            $imagen = $request->file('imagen');
+
+            $nombreImagen = Str::uuid() . "." . $imagen->extension();
+
+            $manager = new ImageManager(new Driver());
+            $imagenServidor = $manager::imagick()->read($imagen);
+            $imagenServidor->resizeDown(1000, 1000);
+
+            $imagenPath = public_path('perfiles') . '/' . $nombreImagen;
+
+            $imagenServidor->toPng()->save($imagenPath);
+
+            /*return response()->json([
+                'imagen' => $nombreImagen,
+            ]);*/
+        }
+
+        //Guardar cambios
+        $usuario = User::find(auth()->user()->id);
+        $usuario->username = $request->username;
+        $usuario->imagen = $nombreImagen ?? auth()->user()->imagen ?? '';
+        $usuario->save();
+
+        //Redireccionar
+        return redirect()->route('posts.index', $usuario->username);
+    }
+}
